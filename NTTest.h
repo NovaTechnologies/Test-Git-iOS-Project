0c18252dff88318e7cae9748e50023737bfff625
P <GoldenVagabond@MacBook-Air-di-patrizio.local>
2014-06-27 16:35:33 +0200

    added a test class

---
+++ NTTest.h
@@ -0,0 +1,13 @@
+//
+//  NTTest.h
+//  Git Test
+//
+//  Created by P on 27/06/14.
+//  Copyright (c) 2014 Nova Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NTTest : NSObject
+
+@end
---
+++ NTTest.m
@@ -0,0 +1,13 @@
+//
+//  NTTest.m
+//  Git Test
+//
+//  Created by P on 27/06/14.
+//  Copyright (c) 2014 Nova Technologies. All rights reserved.
+//
+
+#import "NTTest.h"
+
+@implementation NTTest
+
+@end
